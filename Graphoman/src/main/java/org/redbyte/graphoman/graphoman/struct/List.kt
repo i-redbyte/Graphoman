ebf409290793693d8069a07ee713b76b61e82de1
Ilya Sokolov <developer.sokolov@gmail.com>
2022-04-25 19:08:49 +0300

    extended foldLeft for List; added method init, reverse, drop

--- Graphoman/src/main/java/org/redbyte/graphoman/graphoman/struct/List.kt
+++ Graphoman/src/main/java/org/redbyte/graphoman/graphoman/struct/List.kt
@@ -3,11 +3,16 @@ package org.redbyte.graphoman.graphoman.struct
 sealed class List<out A> {
     abstract fun isEmpty(): Boolean
 
+    abstract fun init(): List<A>
+
     abstract fun <B> foldLeft(
         identity: B,
         p: (B) -> Boolean,
-        f: (B) -> (A) -> B
-    ): B
+        f: (B) -> (A) -> B): B
+
+    abstract fun <B> foldLeft(
+        identity: B, zero: B,
+        f: (B) -> (A) -> B): Pair<B, List<A>>
 
     internal object Nil : List<Nothing>() {
         override fun toString(): String = "[NIL]"
@@ -15,6 +20,11 @@ sealed class List<out A> {
         override fun isEmpty(): Boolean = true
 
         override fun <B> foldLeft(identity: B, p: (B) -> Boolean, f: (B) -> (Nothing) -> B): B = identity
+
+        override fun <B> foldLeft(identity: B, zero: B, f: (B) -> (Nothing) -> B):
+                Pair<B, List<Nothing>> = Pair(identity, Nil)
+
+        override fun init(): List<Nothing> = throw IllegalStateException("You can't call init on an empty list")
     }
 
     internal class Exist<out A>( // TODO: Red_byte 17.04.2022 rename class?
@@ -38,16 +48,35 @@ sealed class List<out A> {
             return foldLeft(identity, this)
         }
 
+        override fun <B> foldLeft(identity: B, zero: B, f: (B) -> (A) -> B): Pair<B, List<A>> {
+            tailrec fun <B> foldLeft(acc: B, zero: B, list: List<A>, f: (B) -> (A) -> B): Pair<B, List<A>> = when (list) {
+                Nil -> Pair(acc, list)
+                is Exist ->
+                    if (acc == zero)
+                        Pair(acc, list)
+                    else
+                        foldLeft(f(acc)(list.head), zero, list.tail, f)
+            }
+            return foldLeft(identity, zero, this, f)
+        }
+
         private tailrec fun toString(s: String, list: List<A>): String = when (list) {
             Nil -> s
             is Exist -> toString("$s${list.head}, ", list.tail)
         }
 
+        override fun init(): List<A> = reverse().drop(1).reverse()
     }
 
     fun addToHead(a: @UnsafeVariance A): List<A> = Exist(a, this)
 
+    fun <B> foldLeft(identity: B, f: (B) -> (A) -> B): B = foldLeft(identity, this, f)
+
     operator fun plus(a: @UnsafeVariance A): List<A> = addToHead(a)
+
+    fun reverse(): List<A> = foldLeft(Nil as List<A>) { acc -> { acc.addToHead(it) } }
+
+    fun drop(n: Int): List<A> = drop(this, n)
 
     companion object {
         operator fun <A> invoke(vararg values: A): List<A> =
@@ -60,5 +89,10 @@ sealed class List<out A> {
                 Nil -> acc
                 is Exist -> foldLeft(f(acc)(list.head), list.tail, f)
             }
+
+        tailrec fun <A> drop(list: List<A>, n: Int): List<A> = when (list) {
+            Nil -> list
+            is Exist -> if (n <= 0) list else drop(list.tail, n - 1)
+        }
     }
 }
